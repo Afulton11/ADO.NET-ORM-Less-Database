2e875d8fad59cf38ef40d6ff0123fb7dfcee7b69
Andrew Fulton <afulton20@gmail.com>
2019-04-09 22:20:55 -0500

    Add Execution methods to database contract

--- Data/IDatabase.cs
+++ Data/IDatabase.cs
@@ -1,14 +1,24 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 
 namespace DatabaseFactory.Data.Contracts
 {
     public interface IDatabase
     {
-       IDbConnection CreateConnection();
-       IDbCommand CreateCommand();
-       IDbConnection CreateOpenConnection();
-       IDbCommand CreateCommand(string commandText, IDbConnection connection);
-       IDbCommand CreateStoredProcCommand(string procName, IDbConnection connection);
-       IDataParameter CreateParameter(string parameterName, object parameterValue);
+        IDbConnection CreateConnection();
+        IDbCommand CreateCommand();
+        IDbConnection CreateOpenConnection();
+        IDbCommand CreateCommand(string commandText, IDbConnection connection);
+        IDbCommand CreateStoredProcCommand(string procName, IDbConnection connection);
+        IDataParameter CreateParameter(string parameterName, object parameterValue);
+
+        void TryExecuteTransaction(Action<IDbTransaction> transactionAction);
+        void TryRollback(IDbTransaction transaction);
+
+        void ExecuteReader(IDbCommand command, Action<IDataReader> onRead);
+
+        T ExecuteScalar<T>(IDbCommand command);
+
+        int Execute(IDbCommand command);
     }
 }
